1816e09ba18fa0de6c50f4da9bc6104950c123e8
LeandroNegretti <negrettileandro3@gmail.com>
2024-08-26 18:37:05 -0300

    modelo de animal criado com sequelize

---
+++ src/models/AnimalModel.js
@@ -0,0 +1,64 @@
+import { Sequelize } from "sequelize";
+import db from "../db.js";
+
+export default db.define("animal", {
+  id: {
+    type: Sequelize.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  especie: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  raca: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  idade: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+  sexo: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  tamanho: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  cor: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  caracteristicas: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  status: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  data_entrada: {
+    type: Sequelize.DATE,
+    allowNull: true,
+    defaultValue: Sequelize.NOW,
+  },
+  foto: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  id_abrigo: {
+    type: Sequelize.INTEGER,
+    allowNull: false, 
+    references: {
+      model: "Abrigo", // Nome da tabela de referência
+      key: "id", // Nome da coluna de referência na tabela de Abrigo
+    },
+  },
+});
