97e6dcb7a2d0a7332405c657e06da815ef534aea
LeandroNegretti <negrettileandro3@gmail.com>
2024-08-29 17:56:27 -0300

    rotas de animal criadas

---
+++ src/routes/AnimalRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import AnimalController from "../controllers/AnimalController";
+
+const router = express.Router();
+
+// Define as rotas e os métodos HTTP associados.
+router.get("/animal", AnimalController.listarTodosAnimal);
+router.get("/animal/:id", AnimalController.buscarAnimalPorId);
+router.post("/animal", AnimalController.cadastraAnimal);
+router.put("/animal/:id", AnimalController.atualizarAnimal);
+router.delete("/animal/:id",AnimalController.deletarAnimal);
+
+export default router;
