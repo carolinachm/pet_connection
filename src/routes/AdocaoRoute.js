35fa9698fb177531e5d4427fc92bf30970707325
LeandroNegretti <negrettileandro3@gmail.com>
2024-09-01 19:49:35 -0300

    rotas http de adocao criadas

---
+++ src/routes/AdocaoRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import AdocaoController from "../controllers/AdocaoController";
+
+const router = express.Router()
+
+// Define as rotas e os métodos HTTP associados.
+router.get("/adocao", AdocaoController.listarTodasAdocoes);
+router.get("/adocao/:id", AdocaoController.buscarAdocaoPorId);
+router.post("/adocao", AdocaoController.cadastrarAdocao);
+router.put("/adocao/:id", AdocaoController.atualizarAdocao);
+router.delete("/adocao/:id", AdocaoController.deletarAdocao);
+
+export default router;
