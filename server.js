6b8a57007e6f102bb66c82688a35980ec5ed1a1f
LeandroNegretti <negrettileandro3@gmail.com>
2024-09-18 17:48:04 -0300

    update: cors instalado e adicionado

--- server.js
+++ server.js
@@ -5,19 +5,22 @@ import db from "./src/db/db.js";
 // Importa as rotas
 import usuarioRoutes from "./src/routes/UsuarioRoute.js";
 import animalRoutes from "./src/routes/AnimalRoute.js"
+import cors from 'cors'
 
 // Cria uma instância do Express.
 const app = express();
+
 
 // Adiciona middleware para analisar o corpo das requisições como JSON.
 app.use(express.json());
 
 // Sincroniza o banco de dados e exibe uma mensagem no console.
 db.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`));
+app.use(cors());
 
 // Define as rotas
-app.use("/api", usuarioRoutes);
-app.use("/api", animalRoutes);
+app.use("/api/usuario", usuarioRoutes);
+app.use("/api/animal", animalRoutes);
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console.
 app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
